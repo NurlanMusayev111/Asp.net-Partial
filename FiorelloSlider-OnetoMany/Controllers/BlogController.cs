a57bbae21066acf24604fdb6fe89afc6ba4f9d1f
nurlanmusayev <nurlan.musayev.2002@inbox.ru>
2024-05-11 01:16:09 +0400

    Partial View Practice

---
+++ FiorelloSlider-OnetoMany/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using FiorelloSlider_OnetoMany.Data;
+using FiorelloSlider_OnetoMany.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorelloSlider_OnetoMany.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Blog> blogs = await _context.Blogs.Where(m => !m.SoftDeleted).ToListAsync();
+            return View(blogs);
+        }
+    }
+}
---
+++ FiorelloSlider-OnetoMany/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace FiorelloSlider_OnetoMany.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
